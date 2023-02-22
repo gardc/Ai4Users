3f77708f1a4e4290a67a9a4c159f5f1d97f385e9
Anh <qapham@stud.ntnu.no>
2023-02-22 10:33:37 +0100

    24 Fix error, simple styling

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Content = (props: any) => {
-
     return (
         <div className="flex justify-center">
-            <div className="max-w-7xl w-full px-5">
-                {props.children}
-            </div>
+            <div className="max-w-7xl w-full">{props.children}</div>
         </div>
-    )
+    );
+};
 
-}
-
-export default Content
+export default Content;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
+import Content from "@/components/Content";
+import InformationDropdownBox from "@/components/InformationDropdownBox";
 import Parent from "@/components/Parent";
 import React from "react";
 
 function index() {
     return (
         <Parent>
-            <div className="index bg-backgroundColor py-32">
-                <p className="text-center text-3xl">Estimation of sick leave</p>
-                <p className="text-center text-m">
-                    We will estimate you total duration of sick leave
-                </p>
-            </div>
+            <Content>
+                <div className="index bg-backgroundColor py-32 flex-col flex items-center">
+                    <p className="text-center text-3xl">
+                        Estimation of sick leave
+                    </p>
+                    <p className="text-center text-m">
+                        We will estimate you total duration of sick leave
+                    </p>
+                    <InformationDropdownBox
+                        title={"Information box"}
+                        initialInfo={"Lorem ipsum dette er initial info"}
+                        extendedInfo={"Lorem ipsum dette er extendedinfo"}
+                    />
+                </div>
+            </Content>
         </Parent>
     );
 }
