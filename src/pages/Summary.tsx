e46ee737907db741941ae14ece370d6ba3b4164b
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-01 17:00:03 +0100

    Fix background color and comment out button
    Button for incorrect  info refundant for user testing.
    
    #29

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -31,54 +31,55 @@ const Summary: React.FC = () => {
     return (
         <Parent>
             <Content>
-                <div className="bg-gradient-to-b from-sky-blue to-slate-50">
-                    <div className="flex justify-start py-5 pt-10 text-white">
-                        <Link
-                            className="text-white pl-12 px-3"
-                            href={"/LandingPage"}
-                        >
-                            Frontpage
-                        </Link>
-                        {">"}
-                        <Link className="text-white px-3" href={"/UsingAi"}>
-                            Using AI
-                        </Link>
-                        {">"}
-                        <Link className="text-white px-3" href={"/Summary"}>
-                            Summary
-                        </Link>
-                    </div>
-                    {/*
+                <div className="bg-slate-50 h-screen">
+                    <div className="bg-gradient-to-b from-sky-blue to-slate-50">
+                        <div className="flex justify-start py-5 pt-10 text-white">
+                            <Link
+                                className="text-white pl-12 px-3"
+                                href={"/LandingPage"}
+                            >
+                                Frontpage
+                            </Link>
+                            {">"}
+                            <Link className="text-white px-3" href={"/UsingAi"}>
+                                Using AI
+                            </Link>
+                            {">"}
+                            <Link className="text-white px-3" href={"/Summary"}>
+                                Summary
+                            </Link>
+                        </div>
+                        {/*
                     <div className="flex items-center ml-5">
-                        <Button color="black" href={"/UsingAi"}>
                         <Button color="black" href={"/UsingAi"}>
                             Go back
                         </Button>
                     </div> */}
-                    <div className="flex justify-center text-2xl text-center">
-                        <h1 dangerouslySetInnerHTML={{ __html: text }} />
-                    </div>
+                        <div className="flex justify-center text-2xl text-center">
+                            <h1 dangerouslySetInnerHTML={{ __html: text }} />
+                        </div>
 
-                    <div className="flex justify-center pb-8">
-                        <h2 className="text-lg font-bold text-gray-600 text-center">
-                            Summary of information used to estimate sick leave
-                            duration
-                        </h2>
+                        <div className="flex justify-center pb-8">
+                            <h2 className="text-lg font-bold text-gray-600 text-center">
+                                Summary of information used to estimate sick
+                                leave duration
+                            </h2>
+                        </div>
                     </div>
-                </div>
-                <div className="bg-slate-50">
-                    <div className="flex justify-center pt-4">
-                        <DataTable data={exampleData} />
-                    </div>
-                    <div className="flex justify-center mt-4">
+                    <div className="bg-slate-50">
+                        <div className="flex justify-center pt-4">
+                            <DataTable data={exampleData} />
+                        </div>
+                        {/* <div className="flex justify-center mt-4">
                         <Button color="black" onClick={() => alert("Okay")}>
                             Is this information incorrect?
                         </Button>
-                    </div>
-                    <div className="flex justify-center mt-4">
-                        <Button color="black" href="/estimate">
-                            Estimate sick leave
-                        </Button>
+                    </div> */}
+                        <div className="flex justify-center mt-4">
+                            <Button color="black" href="/estimate">
+                                Estimate sick leave
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </Content>
