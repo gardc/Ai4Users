a67587472aca2632eed648c603a39edaf2aea24c
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-01 16:07:56 +0100

    Remove unused dropdown box in Using AI  page
    #30

--- src/pages/UsingAi.tsx
+++ src/pages/UsingAi.tsx
@@ -312,15 +312,6 @@ const UsingAI = () => {
                     <InformationDropdownBox
                         title={""}
                         initialInfo={
-                            "You have the right to choose whether to use this AI-model or not. If you choose to not use the model the case worker will handle your case without any involvement from the artificial intelligence model. This may result in longer processing time for your case"
-                        }
-                        extendedInfo={
-                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-                        }
-                    />
-                    <InformationDropdownBox
-                        title={""}
-                        initialInfo={
                             <>
                                 <p>
                                     You have the right to choose whether to use
