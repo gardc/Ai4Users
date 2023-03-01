645bf4376d281c68da2e66b2f06ff08b3c16d4be
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-01 19:18:48 +0100

    Change style and text on landing page
    More realistic text.
    
    #29

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,6 +4,7 @@ import Content from "@/components/Content";
 import DataTable from "@/components/DataTable";
 import NavBar from "@/components/NavBar";
 import Parent from "@/components/Parent";
+import InformationDropdownBox from "@/components/InformationDropdownBox";
 
 /**
  * The landing page component that displays initial information about the web
@@ -16,12 +17,16 @@ const LandingPage: React.FC = () => {
         <Parent>
             <Content>
                 {/* <NavBar /> */}
-                <div className="index bg-gradient-to-b from-sky-blue to-slate-50 py-32 flex-col flex items-center">
-                    <p className="text-center text-5xl">
-                        Estimation of sick leave
+                <div className="index bg-gradient-to-b from-sky-blue to-slate-50 pt-24 pb-16 flex-col flex items-center">
+                    <p className="text-center text-prussian-blue font-semibold text-3xl">
+                        Prediction of sick leave duration
                     </p>
-                    <p className="text-center text-m">
-                        We will estimate you total duration of sick leave
+                    <p className="w-3/4 mt-8 text-left">
+                        In order to provide you with appropriate support during
+                        your sick leave, we will make a prediction of its
+                        duration based on your data. This is helpful in planning
+                        for support measures such as follow-up meetings and
+                        economical support for a longer period of time.
                     </p>
                 </div>
                 {/*Second page that shows personal information */}
@@ -30,22 +35,23 @@ const LandingPage: React.FC = () => {
                         className="w-3/4 
                 bg-white drop-shadow-lg py-8 px-16 rounded-xl flex justify-center items-center flex-col m-4"
                     >
-                        <p className="text-2xl font-semibold py-3">
-                            Estimation of sick leave duration
+                        <p className="text-2xl py-3">
+                            Data that is used in the prediction
                         </p>
-                        <p className="py-3">
-                            In order to provide you with the appropriate support
-                            we use this information to estimate the sick leave
-                            duration. There is several parameters that.
+                        <p className="py-3 text-left mb-4 w-4/6">
+                            Below are all the data points about you that will be
+                            used in predicting your sick leave duration. Click
+                            on the "?" beside each data point to see the reason
+                            for its use.
                         </p>
                         {/*Her kommer komponent om informasjon om brukeren */}
                         <DataTable data={exampleData}></DataTable>
                         <div className="flex-col justify-center flex">
-                            <div className="flex justify-center mt-4">
+                            {/* <div className="flex justify-center mt-4">
                                 <Button color="black" href="/%">
                                     Is this information incorrect?
                                 </Button>
-                            </div>
+                            </div> */}
                             <div className="flex justify-center mt-4">
                                 <Button color="black" href="/UsingAi">
                                     Continue
