6653a1c32ab94ff0f5f642cba52e56ee3bddc8d0
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-15 16:15:41 +0100

    Add LocaleSelection to NavBar
    Makes LocaleSelection available on all pages.
    
    #35 Co-authored-by: gardc <gardc@users.noreply.github.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import LocaleSelection from "./LocaleSelection";
 
 const NavBar = () => {
-    return {
+    return (
+        <div className="w-full flex justify-center align-center bg-sky-blue ">
+            <div className="m-4 max-w-4xl flex flex-row justify-between w-full ">
+                {/* Left side */}
+                <div>
+                    <Link
+                        href="/"
+                        className="italic text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-cyan-700"
+                    >
+                        LeaveEstimation
+                    </Link>
+                </div>
+                {/* Right side */}
+                <LocaleSelection />
+            </div>
+        </div>
         /*
         <div className="flex bg-purple-300 text-xl p-12 pb-2 justify-between">
             <div className="text-end">
@@ -15,7 +32,7 @@ const NavBar = () => {
             </div> 
             </div>
         */
-    };
+    );
 };
 
 export default NavBar;
