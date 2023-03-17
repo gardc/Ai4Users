0f86da9746f67e3a0249dca4b6a69a090878888a
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-17 21:38:08 +0100

    Begin implementing i18next in Landing page
    Enabeling translation.
    
    #36

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -20,20 +20,14 @@ const LandingPage: React.FC = (_props: InferGetStaticPropsType<typeof getStaticP
     return (
         <Parent>
             <Content>
-                {/* <NavBar /> */}
                 <div
                     className="index bg-gradient-to-b from-sky-blue to-slate-50 pt-24 pb-16 
                 flex-col flex items-center"
                 >
                     <p className="text-center text-prussian-blue font-semibold text-3xl">
-                        {t("landingPageTitle")}
+                        {t("landingPage.title")}
                     </p>
-                    <p className="w-3/4 mt-8 text-left">
-                        In order to provide you with appropriate support during your sick leave, we
-                        will make a prediction of its duration based on your data. This is helpful
-                        in planning for support measures such as follow-up meetings and economical
-                        support for a longer period of time.
-                    </p>
+                    <p className="w-3/4 mt-8 text-left">{t("landingPage.description")}</p>
                 </div>
                 {/*Second page that shows personal information */}
                 <div className="text-center bg-slate-50 flex justify-center">
@@ -42,14 +36,12 @@ const LandingPage: React.FC = (_props: InferGetStaticPropsType<typeof getStaticP
                 bg-white drop-shadow-lg py-8 px-16 rounded-xl flex justify-center items-center 
                 flex-col m-4"
                     >
-                        <p className="text-2xl py-3">Data that is used in the prediction</p>
+                        <p className="text-2xl py-3">{t("landingPage.dataPointsSummary.title")}</p>
                         <p className="py-3 text-left w-4/6">
-                            Below is all the information about you that will be used in predicting
-                            your sick leave duration. This information is gathered from the national
-                            population register and our registers.
+                            {t("landingPage.dataPointsSummary.description1")}
                         </p>
                         <p className="py-3 text-left mb-4 w-4/6">
-                            Click on the &quot;?&quot; beside each data point to see why it is used.
+                            {t("landingPage.dataPointsSummary.description2")}
                         </p>
                         {/*Her kommer komponent om informasjon om brukeren */}
                         <DataTable data={exampleData}></DataTable>
@@ -61,7 +53,7 @@ const LandingPage: React.FC = (_props: InferGetStaticPropsType<typeof getStaticP
                             </div> */}
                             <div className="flex justify-center mt-4">
                                 <Button color="black" href="/UsingAi">
-                                    Continue
+                                    {t("landingPage.continueButtonText")}
                                 </Button>
                                 {/* <Link href="/LandingPage" locale={otherLocale}>
                   {otherLocale.toUpperCase()}
