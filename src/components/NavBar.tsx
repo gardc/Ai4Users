3f9439f011731a3d491c8dbc6a26d35a335c7699
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-17 23:08:43 +0100

    Fix imports of Navbar
    #36

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React from "react";
 import LocaleSelection from "./LocaleSelection";
+import React from "react";
 
 const NavBar = () => {
     return (
