860818841917dba4af80f30ae8c73d2d8f51b0ec
Anh <qapham@stud.ntnu.no>
2023-03-21 13:51:09 +0100

    Make informationbox responsive

--- src/components/ExpandableInformationBox.tsx
+++ src/components/ExpandableInformationBox.tsx
@@ -21,7 +21,10 @@ const ExpandableInformationBox: React.FC<ExpandableInformationBoxProps> = ({
 
     useEffect(() => {
         const handleOutsideClick = (event: MouseEvent) => {
-            if (expandedRef.current && !expandedRef.current.contains(event.target as Node)) {
+            if (
+                expandedRef.current &&
+                !expandedRef.current.contains(event.target as Node)
+            ) {
                 setExpanded(false);
             }
         };
@@ -55,21 +58,22 @@ const ExpandableInformationBox: React.FC<ExpandableInformationBoxProps> = ({
                     ></div>
                     <div
                         ref={expandedRef}
-                        className="bg-white p-14 w-3/4 rounded-xl fixed top-1/2 left-1/2 transform
-                        -translate-x-1/2 -translate-y-1/2 z-50 overflow-y-auto max-h-full mt-10
-                        max-w-5xl"
+                        className="bg-white p-4 sm:p-14 w-full sm:w-3/4 sm:rounded-xl fixed top-1/2 left-1/2 transform
+                        -translate-x-1/2 -translate-y-1/2 z-50 overflow-y-auto max-h-full sm:mt-10"
                     >
-                        <div className="font-bold mb-6 text-xl w-5/6 xl:w-full text-left text-violet">
-                            {expandedContentTitle}
-                        </div>
-                        <div className="text-black text-left">{expandedContent}</div>
                         <button
                             onClick={handleExpansion}
                             className="text-black hover:text-sky-600
-                            absolute top-14 right-14 underline"
+                            top-14 right-14 underline justify-end"
                         >
                             {expandedCloseButtonText}
                         </button>
+                        <div className="font-bold mb-6 text-xl w-5/6 xl:w-full text-left text-violet">
+                            {expandedContentTitle}
+                        </div>
+                        <div className="text-black text-left">
+                            {expandedContent}
+                        </div>
                     </div>
                 </>
             )}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -17,7 +17,9 @@ import Parent from "@/components/Parent";
  *
  * @returns A React functional component representing the landing page.
  */
-const LandingPage: React.FC = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
+const LandingPage: React.FC = (
+    _props: InferGetStaticPropsType<typeof getStaticProps>
+) => {
     const { t } = useTranslation("common");
     const router = useRouter();
     const { locale } = router;
@@ -30,9 +32,13 @@ const LandingPage: React.FC = (_props: InferGetStaticPropsType<typeof getStaticP
                         {t("pageProgressBar.frontpage")}
                     </p>
                     <p className="text-gray-500">{">"}</p>
-                    <p className="text-gray-500 px-3">{t("pageProgressBar.usingAiPage")}</p>
+                    <p className="text-gray-500 px-3">
+                        {t("pageProgressBar.usingAiPage")}
+                    </p>
                     <p className="text-gray-500">{">"}</p>
-                    <p className="text-gray-500 px-3">{t("pageProgressBar.summaryPage")}</p>
+                    <p className="text-gray-500 px-3">
+                        {t("pageProgressBar.summaryPage")}
+                    </p>
                 </div>
                 <div
                     className="index bg-gradient-to-b from-sky-blue to-slate-50 pt-8 pb-8 
@@ -41,26 +47,41 @@ const LandingPage: React.FC = (_props: InferGetStaticPropsType<typeof getStaticP
                     <p className="text-center text-prussian-blue font-semibold text-3xl">
                         {t("landingPage.title")}
                     </p>
-                    <p className="w-3/4 mt-8 text-left">{t("landingPage.description")}</p>
+                    <p className="w-5/6 sm:w-3/4 mt-8 text-left">
+                        {t("landingPage.description")}
+                    </p>
                 </div>
                 {/*Second page that shows personal information */}
                 <div className="text-center bg-slate-50 flex justify-center">
                     <div
-                        className="w-3/4 
-                bg-white drop-shadow-lg py-8 px-16 rounded-xl flex justify-center items-center 
-                flex-col m-4"
+                        className="w-full sm:w-3/4 
+                bg-white drop-shadow-lg py-8 sm:py-4 px-4 sm:px-16 rounded-xl flex justify-center items-center flex-col m-4"
                     >
-                        <p className="text-2xl py-3">{t("landingPage.dataPointsSummary.title")}</p>
-                        <p className="py-3 text-left w-4/6">
+                        <p className="text-2xl py-3">
+                            {t("landingPage.dataPointsSummary.title")}
+                        </p>
+                        <p className="py-3 text-left md:w-4/6">
                             {t("landingPage.dataPointsSummary.description1")}
                         </p>
-                        <p className="py-3 text-left mb-4 w-4/6">
+                        <p className="py-3 text-left mb-4 md:w-4/6">
                             {t("landingPage.dataPointsSummary.description2")}
                         </p>
                         {/*Here there will be a component with information about the user */}
-                        {locale == "en" ? <DataTable data={exampleDataEn}/> : <></>}
-                        {locale == "no" ? <DataTable data={exampleDataNo}/> : <></>}
-                        {locale == "de" ? <DataTable data={exampleDataDe}/> : <></>}
+                        {locale == "en" ? (
+                            <DataTable data={exampleDataEn} />
+                        ) : (
+                            <></>
+                        )}
+                        {locale == "no" ? (
+                            <DataTable data={exampleDataNo} />
+                        ) : (
+                            <></>
+                        )}
+                        {locale == "de" ? (
+                            <DataTable data={exampleDataDe} />
+                        ) : (
+                            <></>
+                        )}
                         <div className="flex-col justify-center flex">
                             {/* <div className="flex justify-center mt-4">
                                 <Button color="black" href="/%">
