75f65f864497587e1da7e7c864fc4c7c73a41cde
mats ellingsen <matsel@stud.ntnu.no>
2023-04-12 13:21:30 +0200

    fixed bug

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -38,7 +38,7 @@ const Feedback: React.FC = (
                                 {t("feedback.description")}
                             </p>
                             
-                        <textarea id="message" rows={6} className="block p-2.5 w-[50%] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder= {t("feedback.placeholder")} ></textarea>
+                        <textarea id="message" rows={6} className="block p-2.5 w-[50%] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder= {t("feedback.placeholder")!} ></textarea>
                             <div className="flex justify-center mt-8 mb-10 sm:m-5">
                             <Button color="lavaorange" href="/UserTestingFinishedPage">
                                 {t("feedback.submit")}
