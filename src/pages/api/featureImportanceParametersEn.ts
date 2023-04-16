31ce0ab85879ccc1db808cf98e2cc36646a1c207
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-04-16 16:03:15 +0200

    Create feature importance data files
    With mock data. USed to show diagram of feature importance.
    
    #70

---
+++ src/pages/api/featureImportanceParametersEn.ts
@@ -0,0 +1,240 @@
+/**
+ * As of now only manages one parameter that can be changed.
+ */
+
+export const featureImportanceParametersEn = {
+    label: "Age",
+    argument: [
+        {
+            itemName: "16-19",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.35,
+                },
+                {
+                    feature: "County",
+                    value: 0.18,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.21,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.26,
+                },
+            ],
+        },
+        {
+            itemName: "20-24",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.21,
+                },
+                {
+                    feature: "County",
+                    value: 0.11,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.2,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.48,
+                },
+            ],
+        },
+        {
+            itemName: "25-29",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.3,
+                },
+                {
+                    feature: "County",
+                    value: 0.2,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.4,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.1,
+                },
+            ],
+        },
+        {
+            itemName: "30-34",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.15,
+                },
+                {
+                    feature: "County",
+                    value: 0.15,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.4,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.3,
+                },
+            ],
+        },
+        {
+            itemName: "35-39",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.25,
+                },
+                {
+                    feature: "County",
+                    value: 0.25,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.25,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.25,
+                },
+            ],
+        },
+        {
+            itemName: "40-44",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.25,
+                },
+                {
+                    feature: "County",
+                    value: 0.1,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.1,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.55,
+                },
+            ],
+        },
+        {
+            itemName: "45-49",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.3,
+                },
+                {
+                    feature: "County",
+                    value: 0.25,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.2,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.25,
+                },
+            ],
+        },
+        {
+            itemName: "50-54",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.1,
+                },
+                {
+                    feature: "County",
+                    value: 0.1,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.1,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.6,
+                },
+            ],
+        },
+        {
+            itemName: "55-59",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.05,
+                },
+                {
+                    feature: "County",
+                    value: 0.25,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.4,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.3,
+                },
+            ],
+        },
+        {
+            itemName: "60-64",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.2,
+                },
+                {
+                    feature: "County",
+                    value: 0.2,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.2,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.4,
+                },
+            ],
+        },
+        {
+            itemName: "65-69",
+            relativeImportance: [
+                {
+                    feature: "Age",
+                    value: 0.3,
+                },
+                {
+                    feature: "County",
+                    value: 0.1,
+                },
+                {
+                    feature: "Gender",
+                    value: 0.1,
+                },
+                {
+                    feature: "Diagnosis",
+                    value: 0.5,
+                },
+            ],
+        },
+    ],
+};
---
+++ src/pages/api/featureImportanceParametersNo.ts
@@ -0,0 +1,240 @@
+/**
+ * As of now only manAlders one parameter that can be changed.
+ */
+
+export const featureImportanceParametersEn = {
+    label: "Alder",
+    argument: [
+        {
+            itemName: "16-19",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.35,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.18,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.21,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.26,
+                },
+            ],
+        },
+        {
+            itemName: "20-24",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.21,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.11,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.2,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.48,
+                },
+            ],
+        },
+        {
+            itemName: "25-29",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.3,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.2,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.4,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.1,
+                },
+            ],
+        },
+        {
+            itemName: "30-34",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.15,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.15,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.4,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.3,
+                },
+            ],
+        },
+        {
+            itemName: "35-39",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.25,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.25,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.25,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.25,
+                },
+            ],
+        },
+        {
+            itemName: "40-44",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.25,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.1,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.1,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.55,
+                },
+            ],
+        },
+        {
+            itemName: "45-49",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.3,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.25,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.2,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.25,
+                },
+            ],
+        },
+        {
+            itemName: "50-54",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.1,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.1,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.1,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.6,
+                },
+            ],
+        },
+        {
+            itemName: "55-59",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.05,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.25,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.4,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.3,
+                },
+            ],
+        },
+        {
+            itemName: "60-64",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.2,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.2,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.2,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.4,
+                },
+            ],
+        },
+        {
+            itemName: "65-69",
+            relativeImportance: [
+                {
+                    feature: "Alder",
+                    value: 0.3,
+                },
+                {
+                    feature: "Fylke",
+                    value: 0.1,
+                },
+                {
+                    feature: "Kjønn",
+                    value: 0.1,
+                },
+                {
+                    feature: "Diagnose",
+                    value: 0.5,
+                },
+            ],
+        },
+    ],
+};
