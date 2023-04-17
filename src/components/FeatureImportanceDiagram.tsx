23eef677814ea5c0cc90426670f07bbc52f92c60
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-04-17 09:15:30 +0200

    Add comments and change names
    Documentation, and more explanatory names.
    
    #70

--- src/components/FeatureImportanceDiagram.tsx
+++ src/components/FeatureImportanceDiagram.tsx
@@ -1,35 +1,114 @@
 import { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
 
+/**
+ * The type definition for the `FeatureImportanceDiagram` component's props.
+ */
 interface FeatureImportanceDiagramProps {
+    /**
+     * The title for the feature importance section.
+     */
     title: string;
+
+    /**
+     * A description of the feature importance.
+     */
     description: string | JSX.Element;
-    parameters: {
+
+    /**
+     * An object for the parameter the user can modify.
+     */
+    parameter: {
+        /**
+         * The label of the parameter the user can modify.
+         */
         label: string;
-        argument: {
-            itemName: string;
-            relativeImportance: {
+
+        /**
+         * A list of objects for the arguments (values) the changable parameter may have.
+         */
+        arguments: {
+            /**
+             * The name of one of the arguments/values the parameter may have.
+             */
+            argumentName: string;
+
+            /**
+             * A list of features that are affected by the change of the parameter value.
+             */
+            featureImportanceGivenArgument: {
+                /**
+                 * The name of the feature.
+                 */
                 feature: string;
-                value: number;
+
+                /**
+                 * The weight of the feature importance, given the chosen argument/value for the
+                 * changable parameter. The sum of all weights for a given
+                 * featureImportanceGivenArgument object should be 1 to display the intended
+                 * charts.
+                 */
+                weight: number;
             }[];
         }[];
     };
 }
 
+/**
+ * A component for displaying feature importance with a pie chart, allowing users to modify one
+ * parameter/feature to see the feature importance of all features, given the selected value.
+ *
+ * @component
+ * @example
+ * title = "A title"
+ * description = "Some description text";
+ * parameter = {
+ *  label: "Changable Feature"
+ *  arguments: [
+ *      {
+ *          argumentName: "Value 1 for changable feature",
+ *          featureImportanceGivenArgument: [
+ *              {
+ *                  feature: "Feature 1"
+ *                  weight: 0.25
+ *              },
+ *              {
+ *                  feature: "Feature 2"
+ *                  weight: 0.25
+ *              },
+ *          ]
+ *      },
+ *      {
+ *          argumentName: "Value 2 for changable feature",
+ *          featureImportanceGivenArgument: [
+ *              {
+ *                  feature: "Feature 1"
+ *                  weight: 0.4
+ *              },
+ *              {
+ *                  feature: "Feature 2"
+ *                  weight: 0.6
+ *              },
+ *          ]
+ *      },
+ *  ],
+ * };
+ * Usage <FeatureImportanceDiagram title={title} description={description} parameter={parameter} />
+ */
 const FeatureImportanceDiagram: React.FC<FeatureImportanceDiagramProps> = ({
     title,
     description,
-    parameters,
+    parameter,
 }) => {
-    const [importanceValuesOfFeatures, setimportanceValuesOfFeatures] = useState<
+    const [importanceWeightsOfFeatures, setImportanceWeightsOfFeatures] = useState<
         {
             feature: string;
-            value: number;
+            weight: number;
         }[]
     >([]);
 
     const pieChartRef = useRef<SVGSVGElement>(null);
-
+    const featureNames = importanceWeightsOfFeatures.map((d) => d.feature);
     const colorsForDiagram = [
         "#003049",
         "#9B2226",
@@ -43,12 +122,17 @@ const FeatureImportanceDiagram: React.FC<FeatureImportanceDiagramProps> = ({
         "#005F73",
     ];
 
-    const featureNames = importanceValuesOfFeatures.map((d) => d.feature);
-
     useEffect(() => {
-        if (importanceValuesOfFeatures.length === 0) {
-            console.log([parameters.argument[0].relativeImportance[0]]);
-            setimportanceValuesOfFeatures(parameters.argument[0].relativeImportance);
+        drawPieChart();
+    });
+
+    /**
+     * Draws the pie chart as an SVG based on the selected value for thechangable parameter.
+     */
+    const drawPieChart = () => {
+        if (importanceWeightsOfFeatures.length === 0) {
+            console.log([parameter.arguments[0].featureImportanceGivenArgument[0]]);
+            setImportanceWeightsOfFeatures(parameter.arguments[0].featureImportanceGivenArgument);
         }
 
         const svg = d3.select(pieChartRef.current);
@@ -60,12 +144,12 @@ const FeatureImportanceDiagram: React.FC<FeatureImportanceDiagramProps> = ({
             colorMapping[name] = colorScale(name);
         });
 
-        const pieChartLayout = d3.pie<{ feature: string; value: number }>().value((d) => d.value);
+        const pieChartLayout = d3.pie<{ feature: string; weight: number }>().value((d) => d.weight);
 
-        const pieChartData = pieChartLayout(importanceValuesOfFeatures);
+        const pieChartData = pieChartLayout(importanceWeightsOfFeatures);
 
         const arcGenerator = d3
-            .arc<d3.PieArcDatum<{ feature: string; value: number }>>()
+            .arc<d3.PieArcDatum<{ feature: string; weight: number }>>()
             .innerRadius(0)
             .outerRadius(150);
 
@@ -83,7 +167,7 @@ const FeatureImportanceDiagram: React.FC<FeatureImportanceDiagramProps> = ({
             .attr("d", arcGenerator)
             .attr("fill", (d) => colorMapping[d.data.feature])
             .append("title")
-            .text((d) => `${d.data.feature}: ${d.data.value}`);
+            .text((d) => `${d.data.feature}: ${d.data.weight}`);
 
         g.selectAll("text")
             .data(pieChartData)
@@ -91,21 +175,27 @@ const FeatureImportanceDiagram: React.FC<FeatureImportanceDiagramProps> = ({
             .append("text")
             .attr("transform", (d) => `translate(${arcGenerator.centroid(d)})`)
             .attr("dy", "0.35em")
-            .text((d) => d.data.value)
+            .text((d) => d.data.weight)
             .style("fill", "white")
             .style("text-anchor", "middle");
-    }, [importanceValuesOfFeatures]);
+    };
 
-    const handleParameterSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    /**
+     * Sets the state holding the feature importance weights of the features, given the selected
+     * value of the changable parameter.
+     *
+     * @param event The event that triggered the change of value for the changable parameter.
+     */
+    const handleparameterelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const targetValue: string = event.target.value;
-        const importanceValuesOfFeatures: { feature: string; value: number }[] =
-            parameters.argument
-                .find((arg) => arg.itemName === targetValue)
-                ?.relativeImportance?.map((ri) => ({
+        const importanceWeightsOfFeatures: { feature: string; weight: number }[] =
+            parameter.arguments
+                .find((arg) => arg.argumentName === targetValue)
+                ?.featureImportanceGivenArgument?.map((ri) => ({
                     feature: ri.feature,
-                    value: ri.value,
+                    weight: ri.weight,
                 })) ?? [];
-        setimportanceValuesOfFeatures(importanceValuesOfFeatures);
+        setImportanceWeightsOfFeatures(importanceWeightsOfFeatures);
     };
 
     return (
@@ -117,14 +207,14 @@ const FeatureImportanceDiagram: React.FC<FeatureImportanceDiagramProps> = ({
                 </div>
 
                 <div className="flex flex-col lg:w-1/2 bg-white rounded-xl pl-12 p-8 mx-auto">
-                    <label className="text-xl font-bold">{parameters.label}</label>
+                    <label className="text-xl font-bold">{parameter.label}</label>
                     <select
                         className="mt-2 p-2 border rounded-lg shadow-md w-full"
-                        onChange={handleParameterSelection}
+                        onChange={handleparameterelection}
                     >
-                        {parameters.argument.map((argument) => (
-                            <option key={argument.itemName} value={argument.itemName}>
-                                {argument.itemName}
+                        {parameter.arguments.map((argument) => (
+                            <option key={argument.argumentName} value={argument.argumentName}>
+                                {argument.argumentName}
                             </option>
                         ))}
                     </select>
