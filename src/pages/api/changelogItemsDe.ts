ba8a04762bd515f462c010a0a72c0a91b71286e7
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-04-27 21:10:35 +0200

    Create dummy changelog files
    Data for the changelog component.
    
    #131

---
+++ src/pages/api/changelogItemsDe.ts
@@ -0,0 +1,32 @@
+export const changeLogItemsEn = [
+    {
+        dateOfChange: "26.04.2023",
+        titleOfChange: "Included County feature",
+        change: 'The AI model now includes the feature "County", meaning that information about where you live will be used in the prediction.',
+    },
+    {
+        dateOfChange: "02.03.2023",
+        titleOfChange: "Parameter adjustments",
+        change: "Some random changes, this is just  to test how the text looks.",
+    },
+    {
+        dateOfChange: "02.03.2023",
+        titleOfChange: "Parameter adjustments",
+        change: "Some random changes, this is just  to test how the text looks.",
+    },
+    {
+        dateOfChange: "02.03.2023",
+        titleOfChange: "Parameter adjustments",
+        change: "Some random changes, this is just  to test how the text looks.",
+    },
+    {
+        dateOfChange: "02.03.2023",
+        titleOfChange: "Parameter adjustments",
+        change: "Some random changes, this is just  to test how the text looks.",
+    },
+    {
+        dateOfChange: "02.03.2023",
+        titleOfChange: "Parameter adjustments",
+        change: "Some random changes, this is just  to test how the text looks.",
+    },
+];
