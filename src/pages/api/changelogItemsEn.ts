c79581ceb0690f223c62abaf4bfae8f824dde17f
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-04-28 12:50:59 +0200

    Change a parameter to be more descriptive.
    #131

--- src/pages/api/changelogItemsEn.ts
+++ src/pages/api/changelogItemsEn.ts
@@ -1,6 +1,6 @@
 /**
  * Data for the changelog component, English translation. Gives the component a list of objects,
- * each object containing a date, title, the text of the change, and a link to another page (can
+ * each object containing a date, title, the description of the change, and a link to another page (can
  * be left empty). These objects represent changes, for example to the AI model, that can be
  * displayed by the changelog component.
  *
@@ -9,13 +9,13 @@
  *  {
  *      dateOfChange: "12.03.2023",
  *      titleOfChange: "Improved training data for county feature",
- *      change: "The AI model has been retrained with ...",
+ *      changeDescription: "The AI model has been retrained with ...",
  *      readMoreLink: "/Changes12032023",
  *  },
  *  {
  *      dateOfChange: "22.02.2023",
  *      titleOfChange: "Increased training data",
- *      change: "The AI model now has access to a larger dataset ...",
+ *      changeDescription: "The AI model now has access to a larger dataset ...",
  *      readMoreLink: "",
  *  },
  * ]
@@ -24,25 +24,29 @@ export const changeLogItemsEn = [
     {
         dateOfChange: "12.03.2023",
         titleOfChange: "Improved training data for county feature",
-        change: "The AI model has been retrained with a more diverse and up-to-date dataset for the county feature. This should result in more accurate predictions of sick leave duration, as the model can better capture regional differences in health outcomes and medical practices.",
+        changeDescription:
+            "The AI model has been retrained with a more diverse and up-to-date dataset for the county feature. This should result in more accurate predictions of sick leave duration, as the model can better capture regional differences in health outcomes and medical practices.",
         readMoreLink: "",
     },
     {
         dateOfChange: "22.02.2023",
         titleOfChange: "Increased training data",
-        change: "The AI model now has access to a larger dataset with more diverse cases, which should improve its accuracy in predicting sick leave durations.",
+        changeDescription:
+            "The AI model now has access to a larger dataset with more diverse cases, which should improve its accuracy in predicting sick leave durations.",
         readMoreLink: "",
     },
     {
         dateOfChange: "15.01.2023",
         titleOfChange: "Improved normalization technique",
-        change: "We have updated the normalization technique used in the AI model to better account for outliers and improve its ability to handle non-standard cases.",
+        changeDescription:
+            "We have updated the normalization technique used in the AI model to better account for outliers and improve its ability to handle non-standard cases.",
         readMoreLink: "",
     },
     {
         dateOfChange: "30.11.2022",
         titleOfChange: "Added new input feature",
-        change: "We have added a new input feature to the AI model, which takes into account the patient's occupation. This should improve the model's accuracy in predicting sick leave durations for individuals in specific professions.",
+        changeDescription:
+            "We have added a new input feature to the AI model, which takes into account the patient's occupation. This should improve the model's accuracy in predicting sick leave durations for individuals in specific professions.",
         readMoreLink: "",
     },
 ];
