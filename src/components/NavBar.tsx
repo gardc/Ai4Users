292e8a92b54fe74edf1d425cc666f25137d39578
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-04-28 11:42:42 +0200

    Add dynamic changlelog to navbar
    Enable ability to change title and different files based on locale.
    
    #131

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,30 @@ import Link from "next/link";
 import LocaleSelection from "./LocaleSelection";
 import React from "react";
 import SocialServiceLogo from "./SocialServiceLogo";
-import AiModelChangelog from "./AiModelChangelog";
+import AiModelChangelog from "./Changelog";
 import { changeLogItemsEn } from "@/pages/api/changelogItemsEn";
+import router from "next/router";
+import { changeLogItemsNo } from "@/pages/api/changelogItemsNo";
+import { changeLogItemsDe } from "@/pages/api/changelogItemsDe";
 
 interface NavBarProps {
     enableLinkToFrontPage: boolean;
+    enableChangelog: boolean;
+    changelogTitle: string;
+    changelogItems: {
+        dateOfChange: string;
+        titleOfChange: string;
+        change: string;
+        readMoreLink: string;
+    }[];
 }
 
-const NavBar = ({ enableLinkToFrontPage }: NavBarProps) => {
+const NavBar = ({
+    enableLinkToFrontPage,
+    enableChangelog,
+    changelogTitle,
+    changelogItems,
+}: NavBarProps) => {
     return (
         <div className="bg-darkblue ">
             <div className="px-6 sm:px-12 pt-6 pb-6 flex justify-between items-center">
@@ -26,12 +42,15 @@ const NavBar = ({ enableLinkToFrontPage }: NavBarProps) => {
 
                 {/* Right side */}
                 <div className="flex items-center">
-                    <div className="hidden lg:block">
-                        <AiModelChangelog
-                            title={"Recent changes to the AI model"}
-                            listOfChanges={changeLogItemsEn}
-                        />
-                    </div>
+                    {enableChangelog && (
+                        <div className="hidden lg:block">
+                            <AiModelChangelog
+                                title={changelogTitle}
+                                listOfChanges={changelogItems}
+                            />
+                        </div>
+                    )}
+
                     <LocaleSelection />
                 </div>
             </div>
