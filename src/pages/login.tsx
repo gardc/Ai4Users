24c12f1b0d9a1dae156484a6f687c72359db8b53
roberarn <roberarn@stud.ntnu.no>
2023-04-29 15:42:09 +0200

    Improve login page
    - Buttons and text are now in a single column on smaller devices.
    - Added hover effects to LoginButton.tsx
    - Reduced size of frame on login page
    - Added icons to the texts
    - Changed order on the login buttons
    
    #153

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,8 @@ import Container from "@/components/Container";
 import React from "react";
 import NavBar from "@/components/NavBar";
 import LoginButton from "@/components/LoginButton";
+import ClipboardIcon from "@/components/Assets/clipboardIcon";
+import UserIcon from "@/components/Assets/userIcon";
 
 /**
  * The login page component where the user can choose between going to the case handler or citizen
@@ -22,43 +24,65 @@ const Login: React.FC = (
         <Container>
             <div className="bg-darkblue h-screen">
                 <NavBar enableLinkToFrontPage={false} />
-                <div className="index pb-16 flex-col flex items-center h-5/6">
+                <div className="index pb-16 flex-col flex items-center h-2/3">
                     <div
                         className="w-full md:w-5/6 mt-12 h-full
                                     bg-white drop-shadow-lg py-4 sm:py-8 px-4 sm:px-16 rounded-xl
                                     flex justify-center items-center flex-col text-black"
                     >
-                        <div className="mt-6">
+                        <div
+                            className={
+                                "grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-4"
+                            }
+                        >
                             <div
-                                className={"grid grid-cols-2 gap-x-16 gap-y-4"}
+                                className={
+                                    "order-1 flex flex-row md:flex-col items-center"
+                                }
                             >
+                                <div className={"w-20"}>
+                                    <UserIcon />
+                                </div>
                                 <p
                                     className={
-                                        "font-light text-left tracking-wide leading-6 text-lg"
-                                    }
-                                >
-                                    {t("loginPage.caseHandlerDescription")}
-                                </p>
-                                <p
-                                    className={
-                                        "font-light text-left tracking-wide leading-6 text-lg "
+                                        "font-light text-left tracking-wide leading-6 text-lg md:pt-4"
                                     }
                                 >
                                     {t("loginPage.predictSickLeaveDescription")}
                                 </p>
-                                <LoginButton
-                                    color="lightblue"
-                                    href="/caseHandler"
-                                >
-                                    {t("loginPage.caseHandlerLogin")}
-                                </LoginButton>
-                                <LoginButton
-                                    color="lavaorange"
-                                    href="/LandingPage"
-                                >
-                                    {t("loginPage.predictSickLeave")}
-                                </LoginButton>
                             </div>
+                            <div
+                                className={
+                                    "order-3 md:order-2 flex flex-row md:flex-col items-center pt-8 md:pt-0"
+                                }
+                            >
+                                <div className={"w-20"}>
+                                    <ClipboardIcon />
+                                </div>
+                                <p
+                                    className={
+                                        "font-light text-left tracking-wide leading-6 text-lg md:pt-4"
+                                    }
+                                >
+                                    {t("loginPage.caseHandlerDescription")}
+                                </p>
+                            </div>
+                            <LoginButton
+                                color="lavaorange"
+                                href="/LandingPage"
+                                positionSmall={2}
+                                positionLarge={3}
+                            >
+                                {t("loginPage.predictSickLeave")}
+                            </LoginButton>
+                            <LoginButton
+                                color="lightblue"
+                                href="/caseHandler"
+                                positionSmall={4}
+                                positionLarge={4}
+                            >
+                                {t("loginPage.caseHandlerLogin")}
+                            </LoginButton>
                         </div>
                     </div>
                 </div>
