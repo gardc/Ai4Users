784031b6e34dbf3a70443d9b03322cc139bc0608
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-05-04 19:21:19 +0200

    Update colors in Tailwind config
    Remove unused colors, add new ones used.
    
    #163

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,19 +14,13 @@ module.exports = {
     },
     extend: {
       colors: {
-        'primary': "#DCEED1",
-        'secondary': '#AAC0AA',
-        'violet': '#736372',
-        'beaver': '#A18276',
-        'battleship-gray': '#7A918D',
-        'sky-blue': '#8ECAE6',
+        'zinc': '#F4F4F5',
+        'slate-light': '#F8FAFC',
         'prussian-blue': '#003049',
-        'college-blue': '#012347',
         'darkblue':'#001731',
-        'lightblue':'#394a5e',
+        'sky-blue': '#8ECAE6',
         'lavaorange': '#f24922',
         'darkerorange':'#c14922',
-        'grassgreen': '#4db862',
       },
       borderWidth: {
         '1': '1px', 
