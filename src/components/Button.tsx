1442ad7a85e78fea0be46634ceeea2ebfbaed875
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-05-04 19:28:04 +0200

    Remove unused colors and rename colors
    Remove unecessary colors.
    
    #163

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 /**
  * The props for the Button component.
  *
- * @param color - The color of the button. Valid options are "blue", "red", and "white".
+ * @param color - The color of the button. Valid options are "orange", and "blue". Defaults to black if color not provided.
  * @param href - The URL to navigate to when the button is clicked. If provided, the button will be a Link component.
  * @param onClick - The function to call when the button is clicked. If provided, the button will be a regular button element.
  * @param children - The child elements to display inside the button.
  */
 interface ButtonProps {
-    color?: "lavaorange" | "darkblue" | "beaver" | "primary" | "black";
+    color?: "orange" | "blue";
     href?: string;
     onClick?: () => void;
     children: React.ReactNode;
@@ -33,7 +33,7 @@ interface ButtonProps {
  * @returns A React functional component representing a button element or a Link component.
  */
 const Button: React.FC<ButtonProps> = ({
-    color = "lavaorange",
+    color,
     href,
     onClick,
     children,
@@ -45,20 +45,14 @@ const Button: React.FC<ButtonProps> = ({
 
     let colorClasses;
     switch (color) {
-        case "lavaorange":
+        case "orange":
             colorClasses = "bg-lavaorange hover:bg-opacity-50 text-white";
             break;
-        case "darkblue":
+        case "blue":
             colorClasses = "bg-prussian-blue hover:bg-darkblue text-white";
             break;
-        case "primary":
-            colorClasses = "bg-primary text-black border-primary border";
-            break;
-        case "black":
-            colorClasses = "bg-black text-white";
-            break;
         default:
-            colorClasses = "bg-blue-500 text-white";
+            colorClasses = "bg-black text-white";
     }
 
     let statusClass;
@@ -78,12 +72,7 @@ const Button: React.FC<ButtonProps> = ({
     }
 
     return (
-        <button
-            className={classes}
-            onClick={onClick}
-            type={type}
-            disabled={disabled}
-        >
+        <button className={classes} onClick={onClick} type={type} disabled={disabled}>
             {children}
         </button>
     );
