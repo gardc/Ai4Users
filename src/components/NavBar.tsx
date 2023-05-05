86f493ce9c3648e7016b854691e51300fd16d704
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-05-05 12:01:46 +0200

    Remove previous Changelog from Navbar
    #161

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,25 +4,11 @@ import React from "react";
 import SocialServiceLogo from "./SocialServiceLogo";
 import Changelog from "./Changelog";
 
-
 interface NavBarProps {
     enableLinkToFrontPage: boolean;
-    enableChangelog: boolean;
-    changelogTitle: string;
-    changelogItems: {
-        dateOfChange: string;
-        titleOfChange: string;
-        changeDescription: string;
-        readMoreLink: string;
-    }[];
 }
 
-const NavBar = ({
-    enableLinkToFrontPage,
-    enableChangelog,
-    changelogTitle,
-    changelogItems,
-}: NavBarProps) => {
+const NavBar = ({ enableLinkToFrontPage }: NavBarProps) => {
     return (
         <div className="bg-darkblue ">
             <div className="px-6 sm:px-12 pt-6 pb-6 flex justify-between items-center">
@@ -39,15 +25,6 @@ const NavBar = ({
 
                 {/* Right side */}
                 <div className="flex items-center">
-                    {enableChangelog && (
-                        <div className="hidden lg:block">
-                            <Changelog
-                                title={changelogTitle}
-                                listOfChanges={changelogItems}
-                            />
-                        </div>
-                    )}
-
                     <LocaleSelection />
                 </div>
             </div>
